0e895840c57a1a73dc56abb581fdb1caa998474b
adoseof802 <54974289+adoseof802@users.noreply.github.com>
2019-09-11 19:58:47 +0200

    Enhancing the visual elements in the landing page.

--- JS/script.js
+++ JS/script.js
@@ -2,45 +2,45 @@ $(document).ready(
   function()
   {
       $(".facebook").mouseenter(
-        function()
-        {
-            $(".facebook").css("width","3.5%");
-        }
+          function()
+          {
+              $(".facebook").css("width","3.5%");
+          }
       );
 
       $(".facebook").mouseleave(
-        function()
-        {
-            $(".facebook").css("width","3%");
-        }
+          function()
+          {
+              $(".facebook").css("width","3%");
+          }
       );
 
       $(".instagram").mouseenter(
-        function()
-        {
-            $(".instagram").css("width","3.5%");
-        }
+          function()
+          {
+              $(".instagram").css("width","3.5%");
+          }
       );
 
       $(".instagram").mouseleave(
-        function()
-        {
-            $(".instagram").css("width","3%");
-        }
+          function()
+          {
+              $(".instagram").css("width","3%");
+          }
       );
 
       $(".twitter").mouseenter(
-        function()
-        {
-            $(".twitter").css("width","3.5%");
-        }
+          function()
+          {
+              $(".twitter").css("width","3.5%");
+          }
       );
 
       $(".twitter").mouseleave(
-        function()
-        {
-            $(".twitter").css("width","3%");
-        }
+          function()
+          {
+              $(".twitter").css("width","3%");
+          }
       );
   }
 );
