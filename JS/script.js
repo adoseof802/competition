705b2965f5a81fea0b29f8b0600373af99f0361c
adoseof802 <54974289+adoseof802@users.noreply.github.com>
2019-09-11 14:52:38 +0200

    jQuery: When user hovers over social media links, width grows by 0.5%.

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,46 @@
 $(document).ready(
   function()
   {
+      $(".facebook").mouseenter(
+        function()
+        {
+            $(".facebook").css("width","3.5%");
+        }
+      );
+
+      $(".facebook").mouseleave(
+        function()
+        {
+            $(".facebook").css("width","3%");
+        }
+      );
+
+      $(".instagram").mouseenter(
+        function()
+        {
+            $(".instagram").css("width","3.5%");
+        }
+      );
+
+      $(".instagram").mouseleave(
+        function()
+        {
+            $(".instagram").css("width","3%");
+        }
+      );
+
+      $(".twitter").mouseenter(
+        function()
+        {
+            $(".twitter").css("width","3.5%");
+        }
+      );
+
+      $(".twitter").mouseleave(
+        function()
+        {
+            $(".twitter").css("width","3%");
+        }
+      );
   }
 );
